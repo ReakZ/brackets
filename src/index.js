5ec1bd6d8660b03ef3fd709dcda20e18035d24d2
RealZ <12774452+ReakZ@users.noreply.github.com>
2021-03-14 11:17:21 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let pairsOfBrackets = Object.fromEntries(bracketsConfig);
+    let stachOfBrackets = [];
+    let elements = str.split("");
+    for (let element of elements) {
+        if (stachOfBrackets[stachOfBrackets.length - 1] === element) {
+            stachOfBrackets.pop();
+        } else if (pairsOfBrackets[element]) {
+            stachOfBrackets.push(pairsOfBrackets[element]);
+        } else {
+            return false;
+        }
+    }
+    return stachOfBrackets.length === 0;
 }
